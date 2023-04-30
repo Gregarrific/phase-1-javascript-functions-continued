e663d4509c524338d71061ca77a965a3341a7195
Gregory Dutton <greg.dutton@gmail.com>
2023-04-30 14:46:59 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(greeting='roller-skate') {
+    const greetingMessage = `This Saturday, I want to ${greeting}!`;
+    return greetingMessage;
+}
+
+function mondayWork(greeting='go to the office') {
+    const greetingMessage = `This Monday, I will ${greeting}.`;
+    return greetingMessage;
+}
+
+function wrapAdjective(symbol) {
+    const fullMessage = function(adjective = 'special') {
+        return `You are ${symbol}${adjective}${symbol}!`;
+        };
+        return fullMessage;
+}
